8b38b2400ed5c8209b122d879bb82563d55e7cf8
codenoobies <kevindubidaam@gmail.com>
2024-03-07 12:56:40 +0700

    create: Created Login Routes, Root Routes, AuthPage.tsx, index.css && add:added /,/auth, /auth/login routes && add: Created Loader Function to handle data before page load

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import axios from "axios"
+
+// Router DOM
 import {
   createBrowserRouter,
-  RouterProvider
+  redirect,
+  RouterProvider,
 } from "react-router-dom"
+
+// Pages
+import Root from "./assets/routes/Root"
+import Login from "./assets/routes/Login"
+import AuthPage from './assets/components/pages/AuthPage'
+
 
 const router = createBrowserRouter([
   {
     path:"/",
-    id:"root"
+    id:"root",
+    element:<Root/>,
+    loader:async ()=>{
+          try{
+            const data = await axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}auth/userinfo`)
+            return data;
+        }catch(error){
+            return redirect("auth/login")
+        }
+        }
+  },
+  {
+    path:"/auth",
+    id:"auth",
+    element:<AuthPage/>,
+    children:[
+      {
+        path:"/auth/login",
+        id:"login",
+        element: <Login/>,
+        
+      }
+    ]
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
 )
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}","./index.html"],
+  presets: [],
+  darkMode: 'media', // or 'class'
+  theme: {
+    
+  },
+  plugins: [],
+} satisfies Config
+
