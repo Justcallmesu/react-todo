736258af7752f4b92de7830830cc8eecb5761ac6
codenoobies <kevindubidaam@gmail.com>
2024-03-08 12:13:09 +0700

    refactor : Refactored Modal to a standalone components to used in many pages

--- src/assets/components/pages/TodoPage.tsx
+++ src/assets/components/pages/TodoPage.tsx
@@ -1,11 +1,17 @@
 import {Plus} from "react-bootstrap-icons"
 import {useState,useEffect, BaseSyntheticEvent} from 'react'
-import axios from "axios";
-import TodoCard from "../base/Todolist/TodoCard";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+// Components
+import Modal from "../base/Modal";
+import TodoCard from "../base/Todolist/TodoCard";
 
 export default function Todopage({username}:{username:string}){
+    // REDIRECT
     const navigate = useNavigate();
+
+    // STATE
     const [todo, setTodo] = useState([]);
     const [todoField, setText] = useState("");
     
@@ -31,6 +37,9 @@ export default function Todopage({username}:{username:string}){
             callback={GetTodo}
             key={_id}
             isCompleted={isCompleted}
+            setIsModal={setIsModal}
+            setIsError={setIsError}
+            setModalMessage={setModalMessage}
             />
             )
         }
@@ -39,6 +48,9 @@ export default function Todopage({username}:{username:string}){
 
     async function postTodo(){
         if(!todoField){
+            setIsError(true);
+            setIsModal(true);
+            setModalMessage("Todo Cannot be Empty!");
             return;
         }
 
@@ -57,23 +69,44 @@ export default function Todopage({username}:{username:string}){
         return navigate("/auth/login")
     }
 
+    // MODAL
+    const [isModalShown, setIsModal] = useState(false)
+    const [modalMessage, setModalMessage] = useState("")
+    const [modalCallback, setModalCallback] = useState(()=>{})
+    const [isError,setIsError] = useState(false)
+
+    function resetModalState(e:BaseSyntheticEvent){
+        e.stopPropagation();
+        setIsError(false)
+        setModalMessage("")
+        setIsModal(false)
+        setModalCallback(()=>{})
+    }
+
     return(
-        <main className="w-1/2 h-full rounded-lg shadow-2xl relative overflow-hidden">
-            <header className="bg-primary px-5 py-5 text-white flex justify-between items-center">
-                <p className="text-xl">Welcome Back! <span className="font-bold">{username}</span></p>
-                <button className="bg-white text-primary font-bold px-5 py-2 rounded-xl" onClick={HandleLogout}>Log Out</button>
-            </header>
-            <section className="w-full px-5 py-5 flex gap-5">
-                <input type="text" name="" id="" className="TextField" placeholder="What in your mind ?" value={todoField} onChange={HandleTodoField}/>
-                <button className="bg-primary text-white px-2 rounded-lg text-2xl" onClick={postTodo}>
-                    <Plus/>
-                </button>
-            </section>
-            <section className="w-full h-full px-5 overflow-y-auto flex flex-col gap-5">
-                {
-                    BuildTodo()
-                }
-            </section>            
-        </main>
+        <>
+            {
+                isModalShown && 
+                // Modal
+                <Modal isError={isError} modalCallback={modalCallback} resetModalState={resetModalState} modalMessage={modalMessage}/>
+            }
+            <main className="w-1/2 h-full rounded-lg shadow-2xl relative overflow-hidden">
+                <header className="bg-primary px-5 py-5 text-white flex justify-between items-center">
+                    <p className="text-xl">Welcome Back! <span className="font-bold">{username}</span></p>
+                    <button className="bg-white text-primary font-bold px-5 py-2 rounded-xl" onClick={HandleLogout}>Log Out</button>
+                </header>
+                <section className="w-full px-5 py-5 flex gap-5">
+                    <input type="text" name="" id="" className="TextField" placeholder="What in your mind ?" value={todoField} onChange={HandleTodoField}/>
+                    <button className="bg-primary text-white px-2 rounded-lg text-2xl" onClick={postTodo}>
+                        <Plus/>
+                    </button>
+                </section>
+                <section className="w-full h-full px-5 overflow-y-auto flex flex-col gap-5">
+                    {
+                        BuildTodo()
+                    }
+                </section>            
+            </main>
+        </>
     )
 }
